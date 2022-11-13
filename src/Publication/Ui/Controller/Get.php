ea9bf426c46c5db8410993c821c2b8e3c1b6bbfa
Paweł Poloczek <pawel@poloczek.me>
2022-11-14 00:05:31 +0100

    Prepare directories structure

---
+++ src/Publication/Ui/Controller/Get.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyLibrary\Publication\Ui\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Get
+{
+    #[Route('/publications', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        return new JsonResponse([]);
+    }
+}
